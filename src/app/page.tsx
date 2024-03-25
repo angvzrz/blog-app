8390e61325cba8f8d255ebdfa0f7cc55929a4ccb
Angel Valdez <ang_vzrz@live.com.mx>
2024-03-25 01:23:23 -0600

    Added grid styling to home page to use with a group of card elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import { Card } from "./components/ui/card";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className="grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
+      <Card className="w-32 h-24"></Card>
+      <Card className="w-32 h-24"></Card>
+      <Card className="w-32 h-24"></Card>
+      <Card className="w-32 h-24"></Card>
+      <Card className="w-32 h-24"></Card>
+      <Card className="w-32 h-24"></Card>
     </main>
   );
 }
