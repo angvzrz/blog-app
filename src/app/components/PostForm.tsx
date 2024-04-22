124fa3ef137e6364e908945d64856833e72781f2
Angel Valdez <ang_vzrz@live.com.mx>
2024-04-22 01:58:45 -0600

    Adding options to select component. Integrating react-hook-form

--- src/app/components/PostForm.tsx
+++ src/app/components/PostForm.tsx
@@ -1,21 +1,62 @@
+"use client";
+
 import { useForm } from "react-hook-form";
 import { Form } from "./ui/form";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
-import { Select } from "./ui/select";
-import { SelectContent, SelectTrigger } from "@radix-ui/react-select";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+
 import { Button } from "./ui/button";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const formSchema = z.object({
+  postTitle: z.string(),
+  postContent: z.string(),
+  tagOption: z.string(),
+});
 
 export function PostForm() {
-  const form = useForm();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { postTitle: "", postContent: "", tagOption: "" },
+  });
+  const { register, handleSubmit } = { ...form };
+
   return (
     <Form {...form}>
       <form className="flex flex-col items-center justify-center gap-5 mt-10">
-        <Input className="max-w-lg" id="name" placeholder="post title..." />
-        <Textarea className="max-w-lg" placeholder="Post content" />
-        <Select>
-          <SelectTrigger className="max-w-lg">Select tags</SelectTrigger>
-          <SelectContent></SelectContent>
+        <Input
+          {...register("postTitle")}
+          type="text"
+          className="max-w-lg"
+          id="name"
+          placeholder="post title..."
+        />
+        <Textarea
+          {...register("postContent")}
+          className="max-w-lg"
+          placeholder="Post content"
+        />
+        <Select {...register("tagOption")}>
+          <SelectTrigger className="max-w-lg">
+            <SelectValue placeholder="Select a tag" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="react">React</SelectItem>
+              <SelectItem value="next">Next</SelectItem>
+              <SelectItem value="svelte">Svelte</SelectItem>
+              <SelectItem value="angular">Angular</SelectItem>
+            </SelectGroup>
+          </SelectContent>
         </Select>
         <Button className="w-full max-w-lg">Create</Button>
       </form>
