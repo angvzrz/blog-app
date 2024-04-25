40cc126092704d70a5d3d6db4a4662c162aebf13
Angel Valdez <ang_vzrz@live.com.mx>
2024-04-25 01:10:38 -0600

    Pass submit function as prop

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,12 +1,10 @@
-"use client";
+'use client';
 
-import { SubmitHandler } from "react-hook-form";
-import { FormSchema, PostForm } from "../components/PostForm";
+import { SubmitHandler } from 'react-hook-form';
+import { FormSchema, PostForm } from '../components/PostForm/PostForm';
 
 export default function CreatePage() {
-  const handleCreatePost: SubmitHandler<FormSchema> = (data) => {
-    console.log({ data });
-  };
+  const handleCreatePost: SubmitHandler<FormSchema> = (data) => {};
   return (
     <div>
       <h2 className="text-2xl my-4 font-bold text-center">Add new post</h2>
