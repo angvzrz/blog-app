9801f10560477ae261016733649caf5b0e2b08ca
Angel Valdez <ang_vzrz@live.com.mx>
2024-04-28 18:08:09 -0600

    Added blog details page with edit buttons

---
+++ src/app/blog/[id]/page.tsx
@@ -0,0 +1,13 @@
+import { ButtonAction } from '@/app/components/ButtonAction';
+
+export default function BlogDetailsPage() {
+  return (
+    <div>
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold my-4">Post One</h2>
+      </div>
+      <p className="text-slate-700">Post one content</p>
+      <ButtonAction />
+    </div>
+  );
+}
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,13 +1,16 @@
-import Link from "next/link";
-import { Card, CardHeader, CardTitle } from "./ui/card";
-import { Button } from "./ui/button";
+import Link from 'next/link';
+import { Card, CardHeader, CardTitle } from './ui/card';
+import { Button } from './ui/button';
+import { BookOpenCheck } from 'lucide-react';
 
 export function Navbar() {
   return (
     <Card className="w-full border-primary">
       <CardHeader>
         <CardTitle className="flex justify-between items-center">
-          <Link href="/">Blog App</Link>
+          <Link href="/">
+            <BookOpenCheck />
+          </Link>
           <Link href="/create" passHref>
             <Button>Create Post</Button>
           </Link>
