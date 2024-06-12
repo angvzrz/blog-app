a2d4d02454022a8c6f561d347a5f7f59162c4e58
Angel Valdez <ang_vzrz@live.com.mx>
2024-06-12 01:25:52 -0600

    Use get post by id

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -2,10 +2,28 @@
 
 import { SubmitHandler } from 'react-hook-form';
 import { PostForm } from '@/app/components/PostForm/PostForm';
+import { useQuery } from '@tanstack/react-query';
 import { Form } from '@/types/types';
+import axios from 'axios';
 
-export default function EditPostPage() {
+interface EditPostPageProps {
+  readonly params: {
+    id: string;
+  };
+}
+
+export default function EditPostPage({ params }: EditPostPageProps) {
+  const { data } = useQuery({
+    queryKey: ['posts', params.id],
+    queryFn: async () => {
+      const { id } = params;
+      const response = await axios.get(`/api/posts/${id}`);
+      return response.data;
+    },
+  });
+
   const handleEditPost: SubmitHandler<Form> = (data) => {};
+
   return (
     <div>
       <h2 className="text-2xl my-4 font-bold text-center">Edit post</h2>
