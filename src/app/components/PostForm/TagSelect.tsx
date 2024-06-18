f44d646a65a35fb62853b421b6fef29cd5fe4ec9
Angel Valdez <ang_vzrz@live.com.mx>
2024-06-18 01:24:42 -0600

    Updated form elements to be forward ref in order to receive all props from hook form

--- src/app/components/PostForm/TagSelect.tsx
+++ src/app/components/PostForm/TagSelect.tsx
@@ -7,7 +7,9 @@ import {
   SelectTrigger,
   SelectValue,
 } from '../ui/select';
+import { ControllerRenderProps } from 'react-hook-form';
 import { useSuspenseQuery } from '@tanstack/react-query';
+import { forwardRef } from 'react';
 import axios from 'axios';
 
 const getTags = async () => {
@@ -15,26 +17,28 @@ const getTags = async () => {
   return data;
 };
 
-export function TagSelect() {
-  const { data: tags } = useSuspenseQuery<Tag[]>({
-    queryKey: ['tags'],
-    queryFn: getTags,
-  });
+export const TagSelect = forwardRef<HTMLSelectElement, ControllerRenderProps>(
+  function TagSelect(props, ref) {
+    const { data: tags } = useSuspenseQuery<Tag[]>({
+      queryKey: ['tags'],
+      queryFn: getTags,
+    });
 
-  return (
-    <Select>
-      <SelectTrigger>
-        <SelectValue placeholder="Select a tag" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          {tags.map((tag) => (
-            <SelectItem key={tag.id} value={tag.id}>
-              {tag.name}
-            </SelectItem>
-          ))}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-  );
-}
+    return (
+      <Select {...props} onValueChange={props.onChange}>
+        <SelectTrigger>
+          <SelectValue placeholder="Select a tag" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {tags.map((tag) => (
+              <SelectItem key={tag.id} value={tag.id}>
+                {tag.name}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    );
+  },
+);
