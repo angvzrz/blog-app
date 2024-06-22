a599e0f69bb894b5afd30b4756ebeec08a91c5ef
Angel Valdez <ang_vzrz@live.com.mx>
2024-06-22 01:10:09 -0600

    Added some ui components to storybook

---
+++ src/stories/components/ui/input.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Input } from '@/app/components/ui/input';
+
+
+const meta: Meta<typeof Input> = {
+  title: 'Components/UI/Input',
+  component: Input,
+  parameters: {
+    nextjs: { appDirectory: true },
+    layout: 'centered',
+    tags: ['autodocs'],
+  },
+} satisfies Meta<typeof Input>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    placeholder: 'Placeholder',
+  },
+};
