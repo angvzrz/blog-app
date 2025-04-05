4e238d9caff8a8d1ccd3115c80f755ecb7b03427
Angel Valdez <ang_vzrz@live.com.mx>
2025-04-05 00:13:50 -0600

    feat: improve header styling

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,11 +5,12 @@ import { BookOpenCheck } from 'lucide-react';
 
 export function Navbar() {
   return (
-    <Card className="w-full border-primary">
+    <Card className="w-full border-b-slate-300 rounded-none">
       <CardHeader>
         <CardTitle className="flex justify-between items-center">
-          <Link href="/">
+          <Link href="/" className="flex gap-2">
             <BookOpenCheck />
+            <h1>Blogit</h1>
           </Link>
           <Link href="/create" passHref>
             <Button>Create Post</Button>
